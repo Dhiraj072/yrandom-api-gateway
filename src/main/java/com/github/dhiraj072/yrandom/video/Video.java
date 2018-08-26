128889124236b7d84ff9b4b849d625b89455554b
Dhiraj <dhirajbhatt072@gmail.com>
2018-08-27 00:30:48 +0800

    Add VideoController to handle rest call for video - currently supports only has a dummy request.
    Add Video class to model a video

---
+++ src/main/java/com/github/dhiraj072/yrandom/video/Video.java
@@ -0,0 +1,11 @@
+package com.github.dhiraj072.yrandom.video;
+
+class Video {
+
+  private String name;
+
+  Video(String name) {
+
+    this.name = name;
+  }
+}
---
+++ src/main/java/com/github/dhiraj072/yrandom/video/VideoController.java
@@ -0,0 +1,14 @@
+package com.github.dhiraj072.yrandom.video;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VideoController {
+
+  @RequestMapping(value = "/video/random")
+  Video getRandomVideo() {
+
+    return new Video("name");
+  }
+}
