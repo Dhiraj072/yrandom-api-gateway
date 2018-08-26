2c06a9757e93346d8dd7644c3de5487fdba9de87
Dhiraj <dhirajbhatt072@gmail.com>
2018-08-26 22:41:48 +0800

    Initial commit

---
+++ src/main/java/com/github/dhiraj072/yrandom/YrandomApplication.java
@@ -0,0 +1,12 @@
+package com.github.dhiraj072.yrandom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YrandomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YrandomApplication.class, args);
+	}
+}
