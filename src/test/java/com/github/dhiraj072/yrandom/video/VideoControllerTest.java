dbb7e5a0887ebfb2fd9eb797e17f7ddda35eade1
Dhiraj <dhirajbhatt072@gmail.com>
2018-08-29 23:58:50 +0800

    A very simple unit test for videocontroller

--- src/test/java/com/github/dhiraj072/yrandom/video/VideoControllerTest.java
+++ src/test/java/com/github/dhiraj072/yrandom/video/VideoControllerTest.java
@@ -1,5 +1,16 @@
+package com.github.dhiraj072.yrandom.video;
+
 import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 public class VideoControllerTest {
 
+  VideoController controller = new VideoController();
+
+  @Test
+  public void testGetRandomVideo() {
+
+    assertEquals("name", controller.getRandomVideo().get(0).getName());
+  }
 }
