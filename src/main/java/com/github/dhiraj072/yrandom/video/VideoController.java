cef080906136863c607ad37197f8f9c00b2fbc4f
Dhiraj <dhirajbhatt072@gmail.com>
2018-09-01 19:36:00 +0800

    Update VideoController to use autowired YoutubeManager to get random videos. Remove unused Video object - use google's youtube Video model instead. Update unit tests

--- src/main/java/com/github/dhiraj072/yrandom/video/VideoController.java
+++ src/main/java/com/github/dhiraj072/yrandom/video/VideoController.java
@@ -1,14 +1,24 @@
 package com.github.dhiraj072.yrandom.video;
 
+import com.google.api.services.youtube.model.Video;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VideoController {
 
+  private final YoutubeManager youtubeManager;
+
+  @Autowired
+  public VideoController(YoutubeManager youtubeManager) {
+
+    this.youtubeManager = youtubeManager;
+  }
+
   @RequestMapping(value = "/video/random")
   Video getRandomVideo() {
 
-      return new Video("name");
+    return youtubeManager.getRandomYoutubeVideo();
   }
 }
--- src/test/java/com/github/dhiraj072/yrandom/video/VideoControllerTest.java
+++ src/test/java/com/github/dhiraj072/yrandom/video/VideoControllerTest.java
@@ -1,16 +1,31 @@
 package com.github.dhiraj072.yrandom.video;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
+import com.google.api.services.youtube.model.Video;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class VideoControllerTest {
 
-  private VideoController controller = new VideoController();
+  private YoutubeManager youtubeManager;
+  private VideoController controller;
+
+  @Before
+  public void setUp() {
+
+    youtubeManager = Mockito.mock(YoutubeManager.class);
+    controller = new VideoController(youtubeManager);
+  }
 
   @Test
   public void testGetRandomVideo() {
 
-    assertNotNull(controller.getRandomVideo());
+    Video randomVideo = new Video();
+    randomVideo.setId("randomId");
+    when(youtubeManager.getRandomYoutubeVideo()).thenReturn(randomVideo);
+    assertEquals("randomId", controller.getRandomVideo().getId());
   }
 }
