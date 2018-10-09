513bfd24d106403d0bac70e608f5ffac70c1d637
Dhiraj <dhirajbhatt072@gmail.com>
2018-10-09 22:30:00 +0800

    YoutubeServiceProxy now returns a map of string,object containing the video info which jackson can easily serialize

--- src/main/java/com/github/dhiraj072/yrandom/video/VideoController.java
+++ src/main/java/com/github/dhiraj072/yrandom/video/VideoController.java
@@ -1,6 +1,8 @@
 package com.github.dhiraj072.yrandom.video;
 
-import com.google.api.services.youtube.model.Video;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/yrandom")
 public class VideoController {
 
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(VideoController.class);
+
   @Autowired
   YoutubeServiceProxy youtubeServiceProxy;
 
+  /**
+   * Get a random video
+   * @return Map containing video information, which jackson is able to
+   * serialize to json out of the box
+   */
   @RequestMapping(value = "/video/random")
-  Video getRandomVideo() {
+  Map<String, Object> getRandomVideo() {
 
-    return youtubeServiceProxy.getRandomVideo();
+    Map<String, Object> videoJson = youtubeServiceProxy.getRandomVideo();
+    LOGGER.info("Got random video {}", videoJson);
+    return videoJson;
   }
 }
