64d9a7c76c396282255023c6103c1ba3c3398f23
Dhiraj <dhirajbhatt072@gmail.com>
2018-10-11 20:30:00 +0800

    Enable load balancing on youtube service using ribbon

--- src/main/java/com/github/dhiraj072/yrandom/video/YoutubeServiceProxy.java
+++ src/main/java/com/github/dhiraj072/yrandom/video/YoutubeServiceProxy.java
@@ -1,10 +1,12 @@
 package com.github.dhiraj072.yrandom.video;
 
 import java.util.Map;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name="youtube-service", url = "localhost:8081")
+@FeignClient(name="youtube-service")
+@RibbonClient(name="youtube-service")
 public interface YoutubeServiceProxy {
 
   @GetMapping("/youtube/video/random")
